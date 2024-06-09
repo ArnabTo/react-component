e84fb63635d69a1a7821dff9015750e43fd966f6
ArnabTo <arnabs929@gmail.com>
2024-06-09 10:54:31 +0600

    first commit

---
+++ react-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Faq from './component/Faq'
+import Footer from './component/Footer'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Hello world!</h1>
+     <Faq/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
